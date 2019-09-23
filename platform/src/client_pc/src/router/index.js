a519c67100bdbc8f1d8828b84626934dac8a5fd2
Zhang <zhangtaotao@outlook.com>
2019-09-23 22:06:35 +0800

    完成了注册组件

--- platform/src/client_pc/src/router/index.js
+++ platform/src/client_pc/src/router/index.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import spLogin from '../components/spLogin'
+import spRegister from '../components/spRegister'
 
 // 三方组件需要use
 Vue.use(Router)
@@ -11,5 +12,8 @@ export default new Router({
   routes: [{
     path: '/login',
     component: spLogin
+  }, {
+    path: '/register',
+    component: spRegister
   }]
 })
