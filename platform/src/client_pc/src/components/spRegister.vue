9993f34669d28f1076ad6c48d8a030eecb2b9456
carldu <18556906294@163.com>
2019-09-24 22:48:15 +0800

    代码格式化

--- platform/src/client_pc/src/components/spRegister.vue
+++ platform/src/client_pc/src/components/spRegister.vue
@@ -1,75 +1,74 @@
 <template>
-    <el-form ref="form" :model="form" label-width="90px" class="register">
-        <span class="header">
-            {{header}}
-        </span>
-        <el-form-item label="昵称：" for="name">
-            <el-input v-model="form.name" id="name"></el-input>
-        </el-form-item>
-        <el-form-item label="邮箱：" for="email">
-            <el-input type="email" v-model="form.email" id="email"></el-input>
-            
-        </el-form-item>
-        <el-form-item label="密码：" for="psw">
-            <el-input type="password" v-model="form.password" id="psw"></el-input>
-        </el-form-item>
-        <el-form-item label="确认密码：" for="r_psw">
-            <el-input type="password" v-model="form.re_password" id="r_psw"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button style="width=100%" type="primary" @click="check">注册</el-button>
-        </el-form-item>
-    </el-form>
+  <el-form ref="form" :model="form" label-width="90px" class="register">
+    <span class="header">
+      {{header}}
+    </span>
+    <el-form-item label="昵称：" for="name">
+      <el-input v-model="form.name" id="name"></el-input>
+    </el-form-item>
+    <el-form-item label="邮箱：" for="email">
+      <el-input type="email" v-model="form.email" id="email"></el-input>
+
+    </el-form-item>
+    <el-form-item label="密码：" for="psw">
+      <el-input type="password" v-model="form.password" id="psw"></el-input>
+    </el-form-item>
+    <el-form-item label="确认密码：" for="r_psw">
+      <el-input type="password" v-model="form.re_password" id="r_psw"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button style="width=100%" type="primary" @click="check">注册</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
-    name: 'spRegister',
-    props:{
-        header:{
-            type: String,
-            default: 'Sign Up'
-        }
-    },
-    data() {
-        return{
-            form:{
-                name: '',
-                email: '',
-                password: '',
-                re_password: ''
-            }
-        }
-    },
-    methods: {
-        check() {
-            let errorMsg = '';
-            if(this.form.name === ''){
-                errorMsg = '账号不能为空！';
-            }
-            else if(this.form.email === ''){
-                errorMsg = '邮箱不能为空！';                
-            }     
-            else if (!(/^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/.test(this.form.email)))
-            {
-                errorMsg = '邮箱错误!';
-            }       
-            else if(this.form.password === ''){
-                errorMsg = '密码不能为空！';
-            }
-            else if(this.form.re_password === ''){
-                errorMsg = '确认密码不能为空！';
-            }
-            else if(this.form.password != this.form.re_password){
-                errorMsg = '前后密码不一致！';
-            }
-            else{
-                errorMsg = '注册成功！';
-            }
-            alert(errorMsg);
-        }
-        
+  name: 'spRegister',
+  props: {
+    header: {
+      type: String,
+      default: 'Sign Up'
     }
+  },
+  data () {
+    return {
+      form: {
+        name: '',
+        email: '',
+        password: '',
+        re_password: ''
+      }
+    }
+  },
+  methods: {
+    check () {
+      let errorMsg = '';
+      if (this.form.name === '') {
+        errorMsg = '账号不能为空！';
+      }
+      else if (this.form.email === '') {
+        errorMsg = '邮箱不能为空！';
+      }
+      else if (!(/^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/.test(this.form.email))) {
+        errorMsg = '邮箱错误!';
+      }
+      else if (this.form.password === '') {
+        errorMsg = '密码不能为空！';
+      }
+      else if (this.form.re_password === '') {
+        errorMsg = '确认密码不能为空！';
+      }
+      else if (this.form.password != this.form.re_password) {
+        errorMsg = '前后密码不一致！';
+      }
+      else {
+        errorMsg = '注册成功！';
+      }
+      alert(errorMsg);
+    }
+
+  }
 }
 </script>
 
@@ -84,6 +83,6 @@ export default {
 .register {
   width: 400px;
   height: 400px;
-  text-align: center;;
+  text-align: center;
 }
 </style>
