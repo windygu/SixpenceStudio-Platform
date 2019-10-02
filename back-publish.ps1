258e2fbad33c6d40aee31974353c139f60f317dc
18556906294@163.com <carldu@rektec.com.cn>
2019-10-03 01:08:43 +0800

    添加后端发包脚本

--- back-publish.ps1
+++ back-publish.ps1
@@ -1,16 +1,9 @@
-
-Write-Message "Nuget restoring ..."
 nuget restore platform\src\dotnet\SixpenceStudio-Platform\SixpenceStudio-Platform.sln
-$buildException = MSBuild.exe platform\src\dotnet\SixpenceStudio-Platform\SixpenceStudio-Platform.sln
+$buildException = MSBuild.exe "platform\src\dotnet\SixpenceStudio-Platform\SixpenceStudio-Platform.sln"  /t:rebuild  /p:Configuration=Release
 If (! $?) { Throw $buildException }
-Write-Success-Message "Rebuild success."
 
-Write-Message "Clearing build folder ..."
 If (Test-Path platform\build) { Remove-Item -Recurse -Force "platform\build\*.nupkg" }
-Write-Success-Message "Clear build folder successfully.
 
-nuget pack platform\src\dotnet\SixpenceStudio-Platform\Platform.Core\Platform.Core.csproj -Version 2.1.0 -Properties Configuration=Release
-Write-Success-Message "Nuget pack success."
+nuget pack platform\src\dotnet\SixpenceStudio-Platform\Platform.Core\Platform.Core.csproj -Version 1.0.0 -Properties Configuration=Release -OutputDirectory "platform\build"
 
-Write-Message "Nuget pushing ..."
-nuget push platform\build\*.nupkg -Source http://www.dumiaoxin.top:8001/nuget
+nuget push platform\build\*.nupkg -Source http://www.dumiaoxin.top:8001 -ApiKey 9F2E9384-F50A-43FF-8BA9-5D5E981C6561
