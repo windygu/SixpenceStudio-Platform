c58a4588739c493c73c615f8439d4ed8f72f0c03
carldu <carldu@rektec.com.cn>
2020-03-08 16:05:30 +0800

    登录页样式改造

--- platform/src/client_pc/src/components/login.vue
+++ platform/src/client_pc/src/components/login.vue
@@ -1,18 +1,20 @@
 <template>
-  <el-form ref="form" :model="data" :rules="rules" label-width="100px" class="login">
-    <el-form-item class="header">
-      <span>{{header}}</span>
-    </el-form-item>
-    <el-form-item prop="account">
-      <el-input v-model="data.account" placeholder="邮箱或者手机号"></el-input>
-    </el-form-item>
-    <el-form-item prop="password">
-      <el-input v-model="data.password" placeholder="密码" show-password></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button style="width:100%" type="primary" @click="signIn" :loading="loading">登录</el-button>
-    </el-form-item>
-  </el-form>
+  <div class="login">
+    <el-form ref="form" :model="data" :rules="rules" class="login__wrapper">
+      <el-form-item>
+        <span class="header">{{header}}</span>
+      </el-form-item>
+      <el-form-item prop="account">
+        <el-input v-model="data.account" placeholder="邮箱或者手机号"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input v-model="data.password" placeholder="密码" show-password></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button style="width:100%" type="primary" @click="signIn" :loading="loading">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -63,17 +65,26 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.header {
-  line-height: 60px;
-  span {
-    width: 100%;
-    font-size: 40px;
-  }
-}
-
 .login {
   width: 400px;
   height: 300px;
-  text-align: center;
+  margin: 0 auto;
+  margin-top: 200px;
+  border: 5px solid #d1d1d1;
+  border-radius: 5%;
+  box-shadow: 10px 10px 20px 10px #d1d1d1,
+    -10px 10px 10px 10px rgba(255, 255, 255, 0.5);
+  .login__wrapper {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    .header {
+      line-height: 60px;
+      font-size: 40px;
+    }
+  }
+}
+/deep/.el-form-item__content {
+  margin: 5px 10px;
 }
 </style>
