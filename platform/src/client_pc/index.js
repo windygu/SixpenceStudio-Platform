99098804930ddfea4267362bdcb8e6f1d1d41552
carldu <carldu@rektec.com.cn>
2020-03-29 13:10:40 +0800

    修改baseUrl取localStorage

--- platform/src/client_pc/index.js
+++ platform/src/client_pc/index.js
@@ -3,6 +3,7 @@ import elementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './src/libs';
 import components from './src/components';
+import axios from 'axios';
 
 // 第三方组件需要use
 Vue.use(elementUI);
@@ -15,5 +16,9 @@ const API = {
   version: process.env.VERSION,
   install
 };
+
+if (!sp.isNullOrEmpty(localStorage.getItem('baseUrl'))) {
+  axios.defaults.baseURL = localStorage.getItem('baseUrl');
+}
 
 export default API;
