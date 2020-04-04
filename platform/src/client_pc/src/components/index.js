b0276f3e9433a53b6ece9f7891931135c0d0546a
Carl Du <carldu@rektec.com.cn>
2020-04-04 10:21:26 +0800

    添加spIcon组件

--- platform/src/client_pc/src/components/index.js
+++ platform/src/client_pc/src/components/index.js
@@ -2,12 +2,14 @@ import register from './register';
 import spBlogTable from './spBlogTable';
 import spHeader from './spHeader';
 import spButtonList from './spButtonList';
+import spIcon from './spIcon';
 
 const components = [
   { name: register.name, component: register },
   { name: spBlogTable.name, component: spBlogTable },
   { name: spHeader.name, component: spHeader },
-  { name: spButtonList.name, component: spButtonList }
+  { name: spButtonList.name, component: spButtonList },
+  { name: spIcon.name, component: spIcon }
 ];
 
 export default components;
