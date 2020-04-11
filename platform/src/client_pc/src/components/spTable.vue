86d43b4dbe208c4f03a9c75a404450747db448c6
carldu <carldu@rektec.com.cn>
2020-04-11 15:02:09 +0800

    修改table组件

--- platform/src/client_pc/src/components/spTable.vue
+++ platform/src/client_pc/src/components/spTable.vue
@@ -3,23 +3,17 @@
     <el-table-column
       v-for="(column, index) in columns"
       :key="index"
-      :type="column.type"
-      :width="column.width || 200"
-      :prop="column.prop"
-      :lable="column.label"
-    ></el-table-column>
-    <el-table-column
-      v-for="(column, index) in columns"
-      :key="index"
       :label="column.label"
       :prop="column.prop"
       :width="column.width"
       :sortable="column.sortable ? 'custom' : false"
     >
       <template slot-scope="scope">
-        <!-- 日期 -->
-        <span v-if="column.type == 'date'">{{ scope.row[column.prop] | moment('YYYY-MM-DD') }}</span>
-        <span v-if="column.type == 'datetime'">{{ scope.row[column.prop] | moment('YYYY-MM-DD HH:MM') }}</span>
+        <span v-if="index == 0">
+          <a class="compute-span" href="javascript:;" @click.stop.prevent="handleClick(scope.row)">{{ scope.row[column.prop] }}</a>
+        </span>
+        <span v-else-if="column.type == 'date'">{{ scope.row[column.prop] | moment('YYYY-MM-DD') }}</span>
+        <span v-else-if="column.type == 'datetime'">{{ scope.row[column.prop] | moment('YYYY-MM-DD HH:MM') }}</span>
         <span v-else>{{ scope.row[column.prop] }}</span>
       </template>
     </el-table-column>
@@ -49,12 +43,20 @@ export default {
   },
   methods: {
     loadData() {
-      this.fetch().then(resp => {
+      this.fetchData().then(resp => {
         this.tableData = resp;
       });
+    },
+    handleClick(row) {
+      this.$emit('link-click', row);
     }
   }
 };
 </script>
 
-<style></style>
+<style lang="less">
+.compute-span {
+  color: #409eff;
+  text-decoration: none;
+}
+</style>
