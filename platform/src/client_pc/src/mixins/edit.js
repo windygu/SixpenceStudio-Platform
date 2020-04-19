ffbb5abf9560c5871b5df375b797023fed6a3629
carldu <carldu@rektec.com.cn>
2020-04-19 16:17:28 +0800

    添加表单验证

--- platform/src/client_pc/src/mixins/edit.js
+++ platform/src/client_pc/src/mixins/edit.js
@@ -28,17 +28,23 @@ export default {
       if (this.preSave && typeof this.preSave === 'function') {
         this.preSave();
       }
-      const operateName = sp.isNullOrEmpty(this.Id) ? 'CreateData' : 'UpdateData';
-      if (sp.isNullOrEmpty(this.Id)) {
-        this.data.Id = sp.newUUID();
-      }
-      sp.post(`api/${this.controllerName}/${operateName}`, this.data).then(() => {
-        if (this.postSave && typeof this.postSave === 'function') {
-          this.postSave();
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          const operateName = sp.isNullOrEmpty(this.Id) ? 'CreateData' : 'UpdateData';
+          if (sp.isNullOrEmpty(this.Id)) {
+            this.data.Id = sp.newUUID();
+          }
+          sp.post(`api/${this.controllerName}/${operateName}`, this.data).then(() => {
+            if (this.postSave && typeof this.postSave === 'function') {
+              this.postSave();
+            }
+            this.$emit('close');
+            this.$emit('load-data');
+            this.$message.success('添加成功');
+          });
+        } else {
+          this.$message.error('请检查表单必填项');
         }
-        this.$emit('close');
-        this.$emit('load-data');
-        this.$message.success('添加成功');
       });
     }
   }
