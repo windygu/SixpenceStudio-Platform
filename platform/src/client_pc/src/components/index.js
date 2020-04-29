04f4948aefd6f08e9716921b6c22591c22af2c8f
carldu <carldu@rektec.com.cn>
2020-04-29 21:50:13 +0800

    去除业务组件

--- platform/src/client_pc/src/components/index.js
+++ platform/src/client_pc/src/components/index.js
@@ -1,5 +1,4 @@
 import register from './register';
-import spBlogTable from './spBlogTable';
 import spHeader from './spHeader';
 import spButtonList from './spButtonList';
 import spIcon from './spIcon';
@@ -8,7 +7,6 @@ import spList from './spList';
 
 const components = [
   { name: register.name, component: register },
-  { name: spBlogTable.name, component: spBlogTable },
   { name: spHeader.name, component: spHeader },
   { name: spButtonList.name, component: spButtonList },
   { name: spIcon.name, component: spIcon },
