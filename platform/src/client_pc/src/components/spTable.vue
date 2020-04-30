16c557e1bbadca01de834686715bad03755d93ef
carldu <carldu@rektec.com.cn>
2020-04-30 08:08:11 +0800

    spTable添加分页

--- platform/src/client_pc/src/components/spTable.vue
+++ platform/src/client_pc/src/components/spTable.vue
@@ -1,29 +1,45 @@
 <template>
-  <el-table ref="table" :data="tableData" style="width: 100%" row-key="Id" @selection-change="handleSelectionChange">
-    <el-table-column type="selection" width="55" v-if="allowSelect"></el-table-column>
-    <el-table-column
-      v-for="(column, index) in columns"
-      :key="index"
-      :label="column.label"
-      :prop="column.prop"
-      :width="column.width"
-      :sortable="column.sortable ? 'custom' : false"
+  <div>
+    <el-table ref="table" :data="tableData" style="width: 100%" row-key="Id" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" v-if="allowSelect"></el-table-column>
+      <el-table-column
+        v-for="(column, index) in columns"
+        :key="index"
+        :label="column.label"
+        :prop="column.prop"
+        :width="column.width"
+        :sortable="column.sortable ? 'custom' : false"
+      >
+        <template slot-scope="scope">
+          <span v-if="index == 0">
+            <a class="compute-span" href="javascript:;" @click.stop.prevent="handleClick(scope.row)">{{ scope.row[column.prop] }}</a>
+          </span>
+          <span v-else-if="column.type == 'date'">{{ scope.row[column.prop] | moment('YYYY-MM-DD') }}</span>
+          <span v-else-if="column.type == 'datetime'">{{ scope.row[column.prop] | moment('YYYY-MM-DD HH:MM') }}</span>
+          <span v-else>{{ scope.row[column.prop] }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      @size-change="sizeChange"
+      @current-change="currentPage"
+      :current-page="pageIndex"
+      :page-count="pageSize"
+      :pager-count="pagerCount"
+      :total="total"
     >
-      <template slot-scope="scope">
-        <span v-if="index == 0">
-          <a class="compute-span" href="javascript:;" @click.stop.prevent="handleClick(scope.row)">{{ scope.row[column.prop] }}</a>
-        </span>
-        <span v-else-if="column.type == 'date'">{{ scope.row[column.prop] | moment('YYYY-MM-DD') }}</span>
-        <span v-else-if="column.type == 'datetime'">{{ scope.row[column.prop] | moment('YYYY-MM-DD HH:MM') }}</span>
-        <span v-else>{{ scope.row[column.prop] }}</span>
-      </template>
-    </el-table-column>
-  </el-table>
+    </el-pagination>
+  </div>
 </template>
 
 <script>
+import pagination from '../mixins/pagination';
+
 export default {
   name: 'sp-table',
+  mixins: [pagination],
   props: {
     columns: {
       type: Array,
@@ -36,6 +52,10 @@ export default {
     allowSelect: {
       type: Boolean,
       default: false
+    },
+    pagination: {
+      type: Boolean,
+      default: true
     }
   },
   data() {
@@ -62,9 +82,13 @@ export default {
 };
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 .compute-span {
   color: #409eff;
   text-decoration: none;
+}
+.el-pagination {
+  text-align: center;
+  padding-top: 20px;
 }
 </style>
