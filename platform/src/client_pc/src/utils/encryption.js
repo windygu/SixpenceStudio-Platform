88a724037c3fe14ab60245ba6ee6771078a95818
carl <18556906294@163.com>
2020-09-09 22:30:23 +0800

    v1.0.87

--- platform/src/client_pc/src/utils/encryption.js
+++ platform/src/client_pc/src/utils/encryption.js
@@ -1,12 +1,6 @@
 export function encryptPwd(str) {
-  const cryptoJS = require('crypto-js');
-  const key = cryptoJS.enc.Utf8.parse('C0536798-3187-47F3-BF34-95596C9338BA'.substr(0, 32));
-  const iv = cryptoJS.enc.Utf8.parse('33998425-3944-4DDE-B3C4-8CAA1681A1B4'.substr(0, 16));
-  const options = {
-    mode: cryptoJS.mode.CBC,
-    padding: cryptoJS.pad.Pkcs7,
-    iv: iv
-  };
-  const ciphertext = cryptoJS.AES.encrypt(str, key, options).toString();
-  return ciphertext;
-}
+  const JSEncrypt = require('jsencrypt');
+  let jse = new JSEncrypt();
+  jse.setPublicKey('C0536798-3187-47F3-BF34-95596C9338BA');
+  return jse.encrypt(str);
+};
