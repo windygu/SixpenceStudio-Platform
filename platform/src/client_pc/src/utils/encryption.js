776e5022380f7f5dd0c74a50d49182ddcd40e6eb
carl <18556906294@163.com>
2020-09-13 18:49:18 +0800

    添加MD5加密方法

--- platform/src/client_pc/src/utils/encryption.js
+++ platform/src/client_pc/src/utils/encryption.js
@@ -1,7 +1,12 @@
-import { JSEncrypt } from 'jsencrypt'
+import { JSEncrypt } from 'jsencrypt';
+import md5 from 'js-md5';
 
 export function encrypt(str, publickKey) {
   const jsencrypt = new JSEncrypt();
   jsencrypt.setPublicKey(publickKey);
   return jsencrypt.encrypt(str);
+};
+
+export function md5Encrypt(str) {
+  return md5(str);
 };
