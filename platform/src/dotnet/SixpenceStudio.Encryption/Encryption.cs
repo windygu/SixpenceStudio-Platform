1dbb5a2d7f570853f4a1faf47197c6c5650040d7
Carl Du <carldu@rektec.com.cn>
2020-09-19 16:50:54 +0800

    添加数据库加密解密工具

---
+++ platform/src/dotnet/SixpenceStudio.Encryption/Encryption.cs
@@ -0,0 +1,53 @@
+﻿using SixpenceStudio.Platform.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SixpenceStudio.Encryption
+{
+    public partial class Encryption : Form
+    {
+        public Encryption()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("请输入需要加密的内容", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            textBox1.Text = DecryptAndEncryptHelper.AESEncrypt(textBox2.Text);
+        }
+
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("请输入需要解密的内容", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            textBox2.Text = DecryptAndEncryptHelper.AESDecrypt(textBox1.Text);
+        }
+    }
+}
