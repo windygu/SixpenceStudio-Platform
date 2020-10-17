c2d9501add219bfc0e4b327034eaa31b6e9b18e0
carl <18556906294@163.com>
2020-10-18 00:23:46 +0800

    去除微信前端

--- platform/src/client_pc/src/module/admin/index.js
+++ platform/src/client_pc/src/module/admin/index.js
@@ -4,13 +4,12 @@ import sysParamGroup from './sysParamGroup';
 import userInfo from './userInfo';
 import job from './job';
 import sysConfig from './sysConfig';
-import wechat from './wechat';
 
 export default [
   {
     path: '/admin',
     name: 'admin',
-    children: [].concat(sysEntity, sysMenu, sysParamGroup, userInfo, job, sysConfig, wechat),
+    children: [].concat(sysEntity, sysMenu, sysParamGroup, userInfo, job, sysConfig),
     meta: { auth: true } // 需要检验
   }
 ];
--- platform/src/client_pc/src/module/admin/wechat/material/index.js
+++
@@ -1,7 +0,0 @@
-export default [
-  {
-    path: '/admin/material',
-    name: 'material',
-    component: () => import('./material')
-  }
-];
