be33135bb3524a1a50e8662046062e09695565c2
carl <18556906294@163.com>
2020-11-16 21:33:51 +0800

    添加job异常捕获

--- platform/src/dotnet/SixpenceStudio.Platform/Job/JobBase.cs
+++ platform/src/dotnet/SixpenceStudio.Platform/Job/JobBase.cs
@@ -47,27 +47,33 @@ namespace SixpenceStudio.Platform.Job
                 var stopWatch = new Stopwatch();
                 stopWatch.Start();
                 var broker = PersistBrokerFactory.GetPersistBroker();
-
-                broker.ExecuteTransaction(() =>
+                try
                 {
+                    broker.ExecuteTransaction(() =>
+                    {
 
-                    Execute(broker);
-                    // 更新下次执行时间
-                    var nextTime = JobHelpers.GetJobNextTime(Name);
-                    var nextTimeSql = "";
-                    var paramList = new Dictionary<string, object>() {
+                        Execute(broker);
+                        // 更新下次执行时间
+                        var nextTime = JobHelpers.GetJobNextTime(Name);
+                        var nextTimeSql = "";
+                        var paramList = new Dictionary<string, object>() {
                         { "@time", DateTime.Now },
                         { "@name", Name }
                     };
 
-                    if (!string.IsNullOrEmpty(nextTime))
-                    {
-                        paramList.Add("@nextTime", Convert.ToDateTime(nextTime));
-                        nextTimeSql = ", nextruntime = @nextTime";
-                    }
+                        if (!string.IsNullOrEmpty(nextTime))
+                        {
+                            paramList.Add("@nextTime", Convert.ToDateTime(nextTime));
+                            nextTimeSql = ", nextruntime = @nextTime";
+                        }
 
-                    broker.Execute($"UPDATE job SET lastruntime = @time {nextTimeSql} WHERE name = @name", paramList);
-                });
+                        broker.Execute($"UPDATE job SET lastruntime = @time {nextTimeSql} WHERE name = @name", paramList);
+                    });
+                }
+                catch (Exception e)
+                {
+                    LogUtils.Error($"作业：{Name}执行异常", e);
+                }
                 stopWatch.Stop();
                 LogUtils.Debug($"作业：{Name} 执行结束，耗时{stopWatch.ElapsedMilliseconds}ms");
             });
