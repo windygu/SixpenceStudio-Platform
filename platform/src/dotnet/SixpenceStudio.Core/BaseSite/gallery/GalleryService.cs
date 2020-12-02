a9744b77cd6c05e4f217640d8b5a6cf99d8764c2
carl <18556906294@163.com>
2020-12-02 21:24:28 +0800

    添加图库

---
+++ platform/src/dotnet/SixpenceStudio.Core/BaseSite/gallery/GalleryService.cs
@@ -0,0 +1,25 @@
+﻿using SixpenceStudio.Core.Data;
+using SixpenceStudio.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixpenceStudio.Core.gallery
+{
+    public class GalleryService : EntityService<gallery>
+    {
+        #region 构造函数
+        public GalleryService()
+        {
+            _cmd = new EntityCommand<gallery>();
+        }
+
+        public GalleryService(IPersistBroker broker)
+        {
+            _cmd = new EntityCommand<gallery>(Broker);
+        }
+        #endregion
+    }
+}
---
+++ platform/src/dotnet/SixpenceStudio.Core/BaseSite/gallery/gallery.cs
@@ -0,0 +1,204 @@
+
+using SixpenceStudio.Core.Entity;
+using System;
+using System.Runtime.Serialization;
+
+
+namespace SixpenceStudio.Core.gallery
+{
+    [EntityName("gallery")]
+    public partial class gallery : BaseEntity
+    {
+        /// <summary>
+        /// 实体id
+        /// </summary>
+        private string _galleryid;
+        [DataMember]
+        public string galleryId
+        {
+            get
+            {
+                return this._galleryid;
+            }
+            set
+            {
+                this._galleryid = value;
+                SetAttributeValue("galleryId", value);
+            }
+        }
+
+
+        /// <summary>
+        /// 标签
+        /// </summary>
+        private string _tags;
+        [DataMember]
+        public string tags
+        {
+            get
+            {
+                return this._tags;
+            }
+            set
+            {
+                this._tags = value;
+                SetAttributeValue("tags", value);
+            }
+        }
+
+
+        /// <summary>
+        /// 预览图
+        /// </summary>
+        private string _preview_url;
+        [DataMember]
+        public string preview_url
+        {
+            get
+            {
+                return this._preview_url;
+            }
+            set
+            {
+                this._preview_url = value;
+                SetAttributeValue("preview_url", value);
+            }
+        }
+
+
+        /// <summary>
+        /// 大图
+        /// </summary>
+        private string _image_url;
+        [DataMember]
+        public string image_url
+        {
+            get
+            {
+                return this._image_url;
+            }
+            set
+            {
+                this._image_url = value;
+                SetAttributeValue("image_url", value);
+            }
+        }
+
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        private string _createdBy;
+        [DataMember]
+        public string createdBy
+        {
+            get
+            {
+                return this._createdBy;
+            }
+            set
+            {
+                this._createdBy = value;
+                SetAttributeValue("createdBy", value);
+            }
+        }
+
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        private string _createdByName;
+        [DataMember]
+        public string createdByName
+        {
+            get
+            {
+                return this._createdByName;
+            }
+            set
+            {
+                this._createdByName = value;
+                SetAttributeValue("createdByName", value);
+            }
+        }
+
+
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        private DateTime? _createdOn;
+        [DataMember]
+        public DateTime? createdOn
+        {
+            get
+            {
+                return this._createdOn;
+            }
+            set
+            {
+                this._createdOn = value;
+                SetAttributeValue("createdOn", value);
+            }
+        }
+
+
+        /// <summary>
+        /// 修改人
+        /// </summary>
+        private string _modifiedBy;
+        [DataMember]
+        public string modifiedBy
+        {
+            get
+            {
+                return this._modifiedBy;
+            }
+            set
+            {
+                this._modifiedBy = value;
+                SetAttributeValue("modifiedBy", value);
+            }
+        }
+
+
+        /// <summary>
+        /// 修改人
+        /// </summary>
+        private string _modifiedByName;
+        [DataMember]
+        public string modifiedByName
+        {
+            get
+            {
+                return this._modifiedByName;
+            }
+            set
+            {
+                this._modifiedByName = value;
+                SetAttributeValue("modifiedByName", value);
+            }
+        }
+
+
+        /// <summary>
+        /// 修改日期
+        /// </summary>
+        private DateTime? _modifiedOn;
+        [DataMember]
+        public DateTime? modifiedOn
+        {
+            get
+            {
+                return this._modifiedOn;
+            }
+            set
+            {
+                this._modifiedOn = value;
+                SetAttributeValue("modifiedOn", value);
+            }
+        }
+
+
+    }
+}
+
