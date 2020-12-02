e66635bd8badd1134c212d14ceeb46f547c3a9e8
Carl Du <carldu@rektec.com.cn>
2020-12-02 13:17:36 +0800

    添加图片压缩方法

--- platform/src/dotnet/SixpenceStudio.Core/Utils/ImageUtil.cs
+++ platform/src/dotnet/SixpenceStudio.Core/Utils/ImageUtil.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,108 @@ namespace SixpenceStudio.Core.Utils
 {
     public static class ImageUtil
     {
+        /// <summary>
+        /// 根据文件流获取Image对象
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
         public static Image GetImage(Stream stream)
         {
             return Image.FromStream(stream);
+        }
+
+        /// <summary>
+        /// 压缩图片至n Kb以下
+        /// </summary>
+        /// <param name="img">图片</param>
+        /// <param name="format">图片格式</param>
+        /// <param name="targetLen">压缩后大小</param>
+        /// <param name="srcLen">原始大小</param>
+        /// <returns>压缩后的图片内存流</returns>
+        public static MemoryStream CompressImage(Image img, ImageFormat format, long targetLen, long srcLen = 0)
+        {
+            //设置允许大小偏差幅度 默认10kb
+            const long nearlyLen = 10240;
+
+            //返回内存流  如果参数中原图大小没有传递 则使用内存流读取
+            var ms = new MemoryStream();
+            if (0 == srcLen)
+            {
+                img.Save(ms, format);
+                srcLen = ms.Length;
+            }
+
+            //单位 由Kb转为byte 若目标大小高于原图大小，则满足条件退出
+            targetLen *= 1024;
+            if (targetLen >= srcLen)
+            {
+                ms.SetLength(0);
+                ms.Position = 0;
+                img.Save(ms, format);
+                return ms;
+            }
+
+            //获取目标大小最低值
+            var exitLen = targetLen - nearlyLen;
+
+            //初始化质量压缩参数 图像 内存流等
+            var quality = (long)Math.Floor(100.00 * targetLen / srcLen);
+            var parms = new EncoderParameters(1);
+
+            //获取编码器信息
+            ImageCodecInfo formatInfo = null;
+            var encoders = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo icf in encoders)
+            {
+                if (icf.FormatID == format.Guid)
+                {
+                    formatInfo = icf;
+                    break;
+                }
+            }
+
+            //使用二分法进行查找 最接近的质量参数
+            long startQuality = quality;
+            long endQuality = 100;
+            quality = (startQuality + endQuality) / 2;
+
+            while (true)
+            {
+                //设置质量
+                parms.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+
+                //清空内存流 然后保存图片
+                ms.SetLength(0);
+                ms.Position = 0;
+                img.Save(ms, formatInfo, parms);
+
+                //若压缩后大小低于目标大小，则满足条件退出
+                if (ms.Length >= exitLen && ms.Length <= targetLen)
+                {
+                    break;
+                }
+                else if (startQuality >= endQuality) //区间相等无需再次计算
+                {
+                    break;
+                }
+                else if (ms.Length < exitLen) //压缩过小,起始质量右移
+                {
+                    startQuality = quality;
+                }
+                else //压缩过大 终止质量左移
+                {
+                    endQuality = quality;
+                }
+
+                //重新设置质量参数 如果计算出来的质量没有发生变化，则终止查找。这样是为了避免重复计算情况{start:16,end:18} 和 {start:16,endQuality:17}
+                var newQuality = (startQuality + endQuality) / 2;
+                if (newQuality == quality)
+                {
+                    break;
+                }
+                quality = newQuality;
+            }
+            return ms;
         }
     }
 }
