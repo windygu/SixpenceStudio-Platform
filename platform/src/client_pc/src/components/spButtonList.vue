7f61a6e1448d0c2b128d7d5d62d16052dad13ca4
Carl Du <carldu@rektec.com.cn>
2020-12-05 14:15:31 +0800

    调整按钮组样式

--- platform/src/client_pc/src/components/spButtonList.vue
+++ platform/src/client_pc/src/components/spButtonList.vue
@@ -1,9 +1,17 @@
 <template>
   <sp-header>
     <template v-for="(button, index) in buttons">
-      <a-input-search v-if="button.name === 'search'" placeholder="请输入内容" style="width: 200px;" @search="onSearch" v-bind:key="index" />
-      <a-button v-else-if="button.name === 'more'" type="link" v-bind:key="index" @click="showMore">{{ moreLabel }}</a-button>
-      <a-button v-else :icon="button.icon" @click="handleClick(button)" v-bind:key="index" style="margin-right:10px">{{ button.text }}</a-button>
+      <a-input-search
+        v-if="button.name === 'search'"
+        placeholder="请输入内容"
+        style="width: 200px;margin-right:10px;"
+        @search="onSearch"
+        v-bind:key="index"
+      />
+      <a-button v-else-if="button.name === 'more'" type="link" v-bind:key="index" @click="showMore" style="margin-right:10px;">{{
+        moreLabel
+      }}</a-button>
+      <a-button v-else :icon="button.icon" @click="handleClick(button)" v-bind:key="index" style="margin-right:10px;">{{ button.text }}</a-button>
     </template>
   </sp-header>
 </template>
