bd303982d25bb1aa8869069832e1ecb316af5bd1
zhoumingxuan <602068446@qq.com>
2020-12-10 09:58:28 +0800

    修改图库,点击图片,弹窗查看大图和下载图片按钮

--- platform/src/client_pc/src/module/admin/gallery/gallery.vue
+++ platform/src/client_pc/src/module/admin/gallery/gallery.vue
@@ -1,5 +1,26 @@
 <template>
-  <vue-waterfall-easy style="position: absolute" :imgsArr="dataList" @scrollReachBottom="loadData"></vue-waterfall-easy>
+  <!--@mouseenter="setSortHover" -->
+  <div>
+    <vue-waterfall-easy @click="showModal" :maxCols="4" style="position: absolute;width: 83%" :imgsArr="dataList" @scrollReachBottom="loadData"></vue-waterfall-easy>
+    <div>
+      <a-modal
+        title="图片"
+        :visible="visible"
+        :confirm-loading="confirmLoading"
+        @ok="handleOk"
+        @cancel="handleCancel"
+        width="70%">
+        <div v-if="visible">
+          <img style="width: 100%;height: 100%" :src="imgUrl"/>
+        </div>
+        <template slot="footer">
+          <div v-if="visible">
+            <a-button type="primary" @click="downloadImg">点击下载</a-button>
+          </div>
+        </template>
+      </a-modal>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +31,10 @@ export default {
   components: { vueWaterfallEasy },
   data() {
     return {
+      imgUrl: '',
+      ModalText: '',
+      visible: false,
+      confirmLoading: false,
       isFirstLoad: true,
       busy: false,
       dataList: [],
@@ -30,6 +55,16 @@ export default {
     this.loadData();
   },
   methods: {
+    showModal(event, { index, value }) {
+      // 阻止a标签跳转
+      event.preventDefault()
+      this.imgUrl = ''
+      this.imgUrl = value.infoUrl
+      this.visible = true;
+    },
+    downloadImg () {
+      window.open(this.imgUrl, '_blank')
+    },
     loadData() {
       if (this.loading) {
         return;
@@ -39,7 +74,6 @@ export default {
       if (this.pageSize * this.pageIndex >= this.total && !this.isFirstLoad) {
         return;
       }
-
       this.busy = true;
       if (!this.isFirstLoad) {
         this.pageIndex += 1;
@@ -51,7 +85,8 @@ export default {
               return {
                 src: this.baseUrl + item.preview_url,
                 href: 'https://www.baidu.com/',
-                info: item.name
+                name: item.name,
+                infoUrl: this.baseUrl + item.image_url
               };
             })
           );
@@ -66,3 +101,5 @@ export default {
   }
 };
 </script>
+<style>
+</style>
