3642159487229602d83f692b44953e278215b1fa
carl <18556906294@163.com>
2021-01-24 12:05:30 +0800

    优化断言帮助类

--- platform/src/dotnet/SixpenceStudio.Core/Utils/AssertUtil.cs
+++ platform/src/dotnet/SixpenceStudio.Core/Utils/AssertUtil.cs
@@ -7,33 +7,50 @@ using System.Threading.Tasks;
 
 namespace SixpenceStudio.Core.Utils
 {
+    /// <summary>
+    /// 断言
+    /// </summary>
     public static class AssertUtil
     {
+        /// <summary>
+        /// 检查是否为真
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="errorMessage"></param>
+        /// <param name="errorId"></param>
         public static void CheckBoolean<T>(bool result, string errorMessage, string errorId)
             where T : Exception
         {
             if (result)
             {
-                var ex = Activator.CreateInstance(typeof(T), errorMessage) as T;
-                if (ex != null)
-                {
-                    LogUtils.Error($"{errorId}：{errorMessage}");
-                    throw ex;
-                }
+                Assert<T>(errorMessage, errorId);
             }
         }
 
+        /// <summary>
+        /// 检查是否是Null或空字符串
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="errorMessage"></param>
+        /// <param name="errorId"></param>
         public static void CheckIsNullOrEmpty<T>(string value, string errorMessage, string errorId)
             where T : Exception
         {
             if (string.IsNullOrEmpty(value))
             {
-                var ex = Activator.CreateInstance(typeof(T), errorMessage) as T;
-                if (ex != null)
-                {
-                    LogUtils.Error($"{errorId}：{errorMessage}");
-                    throw ex;
-                }
+                Assert<T>(errorMessage, errorId);
+            }
+        }
+
+        private static void Assert<T>(string errorMessage, string errorId) where T : Exception
+        {
+            var ex = Activator.CreateInstance(typeof(T), errorMessage) as T;
+            if (ex != null)
+            {
+                LogUtils.Error($"{errorId}：{errorMessage}");
+                throw ex;
             }
         }
     }
