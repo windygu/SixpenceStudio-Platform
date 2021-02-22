eda4ddfcda725432bdbe53e7d34efcb090de79f6
Carl Du <carldu@rektec.com.cn>
2021-02-22 14:45:02 +0800

    优化自动发包脚本

--- backend-publish.ps1
+++ backend-publish.ps1
@@ -1,3 +1,9 @@
+function UpdatePackage($Version) {
+	$Arr = $Version.split(".");
+	$Arr[2] = [convert]::ToInt32($Arr[2], 10) + 1;
+	return $Arr -join ".";
+}
+
 nuget restore SixpenceStudio.Platform.sln
 $buildException = MSBuild.exe "SixpenceStudio.Platform.sln"  /t:rebuild  /p:Configuration=Release
 If (! $?) { Throw $buildException }
@@ -5,12 +11,14 @@ If (! $?) { Throw $buildException }
 If (Test-Path nuget) { Remove-Item -Recurse -Force "nuget\*.nupkg" }
 
 $lastVersion = nuget list SixpenceStudio.Core -Source http://nuget.karldu.cn/nuget
-$lastVersion = $lastVersion -replace "SixpenceStudio.Core ","Current Version: "
-Write-Output $lastVersion
+$lastVersion = $lastVersion -replace "SixpenceStudio.Core ",""
+$newVersion = UpdatePackage $lastVersion
 
-$newVersion = Read-Host "Enter new version"
+Write-Host "New Version: $newVersion"
 
 nuget pack SixpenceStudio.Core\SixpenceStudio.Core.csproj -Version $newVersion -Properties Configuration=Release -OutputDirectory build
 nuget pack SixpenceStudio.WeChat\SixpenceStudio.WeChat.csproj -Version $newVersion -Properties Configuration=Release -OutputDirectory build
 
 nuget push build\*.nupkg -Source http://nuget.karldu.cn/nuget -ApiKey 9F2E9384-F50A-43FF-8BA9-5D5E981C6561
+
+Write-Host "Nuget Publish Success!"
