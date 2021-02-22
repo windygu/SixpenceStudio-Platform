0b0004cc1cad7d7e14b70cae4e521db0e80f93f6
Carl Du <carldu@rektec.com.cn>
2021-02-22 13:30:04 +0800

    重新构建项目目录

--- backend-publish.ps1
+++ backend-publish.ps1
@@ -1,5 +1,5 @@
-nuget restore dotnet\SixpenceStudio.Platform.sln
-$buildException = MSBuild.exe "dotnet\SixpenceStudio.Platform.sln"  /t:rebuild  /p:Configuration=Release
+nuget restore SixpenceStudio.Platform.sln
+$buildException = MSBuild.exe "SixpenceStudio.Platform.sln"  /t:rebuild  /p:Configuration=Release
 If (! $?) { Throw $buildException }
 
 If (Test-Path nuget) { Remove-Item -Recurse -Force "nuget\*.nupkg" }
@@ -10,7 +10,7 @@ Write-Output $lastVersion
 
 $newVersion = Read-Host "Enter new version"
 
-nuget pack dotnet\SixpenceStudio.Core\SixpenceStudio.Core.csproj -Version $newVersion -Properties Configuration=Release -OutputDirectory build
-nuget pack dotnet\SixpenceStudio.WeChat\SixpenceStudio.WeChat.csproj -Version $newVersion -Properties Configuration=Release -OutputDirectory build
+nuget pack SixpenceStudio.Core\SixpenceStudio.Core.csproj -Version $newVersion -Properties Configuration=Release -OutputDirectory build
+nuget pack SixpenceStudio.WeChat\SixpenceStudio.WeChat.csproj -Version $newVersion -Properties Configuration=Release -OutputDirectory build
 
 nuget push build\*.nupkg -Source http://nuget.karldu.cn/nuget -ApiKey 9F2E9384-F50A-43FF-8BA9-5D5E981C6561
